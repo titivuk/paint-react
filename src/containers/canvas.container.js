2c1db89f4eebe55b07a2a8bb88fb019f22f12663
titivuk <titivuk@gmail.com>
2019-03-09 23:26:22 +0300

    add canvas catcher and draw circle example

--- src/containers/canvas.container.js
+++ src/containers/canvas.container.js
@@ -11,8 +11,14 @@ class CanvasContainer extends React.PureComponent {
 	painting = false;
 	currentPosition = null;
 	prevPosition = null;
+	mouseDownPosition = null;
+
 	canvas = null;
+	canvasCatcher = null;
+
 	ctx = null;
+	ctxCatcher = null;
+	canvasCatcherFigure = null;
 
 
 	onMouseDownEvent = (e) => {
@@ -27,6 +33,9 @@ class CanvasContainer extends React.PureComponent {
 
 			this.painting = true;
 			this.currentPosition = {
+				x: offsetX, y: offsetY
+			};
+			this.mouseDownPosition = {
 				x: offsetX, y: offsetY
 			};
 
@@ -56,6 +65,7 @@ class CanvasContainer extends React.PureComponent {
 
 
 	onMouseUpEvent = () => {
+		this.moveFigureFromCatcherToCanvas();
 		this.stopPainting();
 	};
 
@@ -75,29 +85,75 @@ class CanvasContainer extends React.PureComponent {
 	}
 
 
+	moveFigureFromCatcherToCanvas() {
+		const { type, properties } = this.canvasCatcherFigure;
+
+		switch (type) {
+			case 'circle':
+				this.moveCircleFromCatcherToCanvas(properties);
+				break;
+			default:
+				break;
+		}
+	}
+
+
+	moveCircleFromCatcherToCanvas({ centerX, centerY, r }) {
+		this.ctx.beginPath();
+		this.ctx.arc(centerX, centerY, r, 0, 2 * Math.PI);
+		this.ctx.stroke();
+
+		// clear catcher canvas
+		this.ctxCatcher.clearRect(0, 0, this.canvasCatcher.width, this.canvasCatcher.height);
+	}
+
+
 	paint() {
-		if (this.currentPosition === null) {
-			return;
+		const { x: mouseDownX, y: mouseDownY } = this.mouseDownPosition;
+		const { x: currX, y: currY } = this.currentPosition;
+
+		const centerX = (currX + mouseDownX) / 2;
+		const centerY = (currY + mouseDownY) / 2;
+		const r = Math.sqrt(Math.abs(mouseDownX - centerX) ** 2 + Math.abs(mouseDownY - centerY) ** 2);
+
+		if (r > 0) {
+			this.ctxCatcher.clearRect(0, 0, this.canvasCatcher.width, this.canvasCatcher.height);
+			this.ctxCatcher.beginPath();
+			this.ctxCatcher.arc(centerX, centerY, r, 0, 2 * Math.PI);
+			this.ctxCatcher.stroke();
+
+			this.canvasCatcherFigure = {
+				properties: {
+					centerX,
+					centerY,
+					r
+				},
+				type: 'circle',
+			}
 		}
 
-		const { x: currentX, y: currentY } = this.currentPosition;
-		const { x: prevX, y: prevY } = this.prevPosition || {};
+		// if (this.currentPosition === null) {
+		// 	return;
+		// }
 
-		/**
-		 * if u just put the mark then draw point as rectangle
-		 * else connect prev and current coordinates with line
-		 */
-		if (!Number.isInteger(prevX) || !Number.isInteger(prevY)) {
-			this.ctx.fillRect(currentX, currentY, 5, 5);
-		} else {
-			this.ctx.beginPath();
-			// Move the the prevPosition of the mouse
-			this.ctx.moveTo(prevX, prevY);
-			// Draw a line to the current position of the mouse
-			this.ctx.lineTo(currentX, currentY);
-			// Visualize the line using the strokeStyle
-			this.ctx.stroke();
-		}
+		// const { x: currentX, y: currentY } = this.currentPosition;
+		// const { x: prevX, y: prevY } = this.prevPosition || {};
+
+		// /**
+		//  * if u just put the mark then draw point as rectangle
+		//  * else connect prev and current coordinates with line
+		//  */
+		// if (!Number.isInteger(prevX) || !Number.isInteger(prevY)) {
+		// 	this.ctx.fillRect(currentX, currentY, 5, 5);
+		// } else {
+		// 	this.ctx.beginPath();
+		// 	// Move the the prevPosition of the mouse
+		// 	this.ctx.moveTo(prevX, prevY);
+		// 	// Draw a line to the current position of the mouse
+		// 	this.ctx.lineTo(currentX, currentY);
+		// 	// Visualize the line using the strokeStyle
+		// 	this.ctx.stroke();
+		// }
 	}
 
 
@@ -136,21 +192,43 @@ class CanvasContainer extends React.PureComponent {
 		this.ctx = this.canvas.getContext('2d');
 		this.ctx.canvas.width = 1000;
 		this.ctx.canvas.height = 600;
-		this.ctx.lineJoin = 'round';
-		this.ctx.lineCap = 'round';
+		// https://developer.mozilla.org/ru/docs/Web/API/CanvasRenderingContext2D/lineJoin
+		this.ctx.lineJoin = 'bevel';
+		// https://developer.mozilla.org/ru/docs/Web/API/CanvasRenderingContext2D/lineCap
+		this.ctx.lineCap = 'butt';
 		this.ctx.lineWidth = 5;
+
+		this.ctxCatcher = this.canvasCatcher.getContext('2d');
+		this.ctxCatcher.canvas.width = 1000;
+		this.ctxCatcher.canvas.height = 600;
+		// https://developer.mozilla.org/ru/docs/Web/API/CanvasRenderingContext2D/lineJoin
+		this.ctxCatcher.lineJoin = 'bevel';
+		// https://developer.mozilla.org/ru/docs/Web/API/CanvasRenderingContext2D/lineCap
+		this.ctxCatcher.lineCap = 'butt';
+		this.ctxCatcher.lineWidth = 5;
+		this.ctxCatcher.strokeStyle = '#f0f';
 	}
 
 
 	render() {
 		return (
-			<canvas className='canvas'
-				onMouseDown={this.onMouseDownEvent}
-				onMouseLeave={this.onMouseLeaveEvent}
-				onMouseMove={this.onMouseMoveEvent}
-				onMouseUp={this.onMouseUpEvent}
-				ref={(ref) => (this.canvas = ref)}
-			/>
+			// <div className="canvas-wrapper">
+			<>
+				<canvas className='canvas'
+					onMouseDown={this.onMouseDownEvent}
+					onMouseLeave={this.onMouseLeaveEvent}
+					onMouseMove={this.onMouseMoveEvent}
+					onMouseUp={this.onMouseUpEvent}
+					ref={(ref) => (this.canvas = ref)}
+				/>
+				<canvas className='canvas canvas-catcher'
+					onMouseDown={this.onMouseDownEvent}
+					onMouseLeave={this.onMouseLeaveEvent}
+					onMouseMove={this.onMouseMoveEvent}
+					onMouseUp={this.onMouseUpEvent}
+					ref={(ref) => (this.canvasCatcher = ref)}
+				/>
+			</>
 		);
 	}
 
